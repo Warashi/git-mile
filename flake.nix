79edf07a81ee33ba5b11688f88a1e7ac88c76a0e
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2025-10-25 02:44:08 +0900

    build: add nix flake for rust development

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            packages = [
+              nixfmt
+              rust-bin.stable.latest.complete
+            ];
+          };
+      }
+    );
+}
