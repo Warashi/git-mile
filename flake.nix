c823e1fd56175d2131e7e53d646a953105a6d59f
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2025-11-06 13:42:49 +0900

    build: use flake-parts

--- flake.nix
+++ flake.nix
@@ -1,54 +1,97 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # keep-sorted start block=yes
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+    };
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    };
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # keep-sorted end
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      rust-overlay,
-      flake-utils,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-      in
-      {
-        packages.default = pkgs.callPackage ./. {
-          rustPlatform =
+    { flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      imports = [
+        inputs.git-hooks.flakeModule
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        {
+          config,
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          pre-commit = {
+            check.enable = true;
+            settings = {
+              src = ./.;
+              hooks = {
+                actionlint.enable = true;
+                treefmt.enable = true;
+              };
+            };
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt = {
+                enable = true;
+                strict = true;
+              };
+              # keep-sorted start
+              keep-sorted.enable = true;
+              pinact.enable = true;
+              # keep-sorted end
+            };
+          };
+
+          packages.default = pkgs.callPackage ./. { };
+
+          devShells.default =
             let
-              toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+              overlays = [ (import inputs.rust-overlay) ];
+              pkgs = import inputs.nixpkgs { inherit system overlays; };
             in
-            pkgs.makeRustPlatform {
-              cargo = toolchain;
-              rustc = toolchain;
+            with pkgs;
+            mkShell {
+              packages = [
+                nixfmt
+                just
+                llvmPackages.libclang
+                clang
+                (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+              ];
+              shellHook = ''
+                export LIBCLANG_PATH=${llvmPackages.libclang.lib}/lib
+              '';
             };
         };
-        devShells.default =
-          with pkgs;
-          mkShell {
-            packages = [
-              nixfmt
-              just
-              llvmPackages.libclang
-              clang
-              (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
-            ];
-            shellHook = ''
-              export LIBCLANG_PATH=${llvmPackages.libclang.lib}/lib
-            '';
-          };
-      }
-    );
+    };
 }
